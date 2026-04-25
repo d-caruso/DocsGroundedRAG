df06e5deaf71a2fb65f54ea3d9a691ae5e97e111
d-caruso <caruso.domenico@gmail.com>
2026-04-25 16:08:56 +0200

    [TASK] 7.6 add error boundary

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,12 +5,15 @@ import '@mantine/code-highlight/styles.css'
 import '@mantine/core/styles.css'
 import './index.css'
 import App from './App.tsx'
+import { ErrorBoundary } from './components/ErrorBoundary'
 import { theme } from './theme'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider defaultColorScheme="auto" theme={theme}>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </MantineProvider>
   </StrictMode>,
 )
